9a54f9e3aa6b4876cfc01bdf220e513b0412066e
Swerik <derswerik@gmail.com>
2020-09-14 15:16:30 +0200

    Cleanup

--- TestGames/FindText/Assets/Scripts/TestShakespeare.cs
+++ TestGames/FindText/Assets/Scripts/TestShakespeare.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using AI;
+using GameAI;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = System.Random;
