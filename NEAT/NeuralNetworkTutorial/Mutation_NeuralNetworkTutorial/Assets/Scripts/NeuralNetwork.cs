724c94568c49e148777b1cb446a1c1055fcc9f39
TheSwerik <derswerik@gmail.com>
2020-09-15 15:09:27 +0200

    cleanup

--- NEAT/NeuralNetworkTutorial/Mutation_NeuralNetworkTutorial/Assets/Scripts/NeuralNetwork.cs
+++ NEAT/NeuralNetworkTutorial/Mutation_NeuralNetworkTutorial/Assets/Scripts/NeuralNetwork.cs
@@ -1,30 +1,27 @@
-﻿
+﻿using System;
 using System.Collections.Generic;
-using System;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 /// <summary>
-/// Neural Network C# (Unsupervised)
+///     Neural Network C# (Unsupervised)
 /// </summary>
 public class NeuralNetwork : IComparable<NeuralNetwork>
 {
-    private int[] layers; //layers
-    private float[][] neurons; //neuron matix
-    private float[][][] weights; //weight matrix
-    private float fitness; //fitness of the network
-
+    private readonly int[] _layers; //layers
+    private float _fitness; //fitness of the network
+    private float[][] _neurons; //neuron matix
+    private float[][][] _weights; //weight matrix
 
     /// <summary>
-    /// Initilizes and neural network with random weights
+    ///     Initilizes and neural network with random weights
     /// </summary>
     /// <param name="layers">layers to the neural network</param>
-    public NeuralNetwork(int[] layers)
+    public NeuralNetwork(IList<int> layers)
     {
         //deep copy of layers of this network 
-        this.layers = new int[layers.Length];
-        for (int i = 0; i < layers.Length; i++)
-        {
-            this.layers[i] = layers[i];
-        }
+        _layers = new int[layers.Count];
+        for (var i = 0; i < layers.Count; i++) _layers[i] = layers[i];
 
 
         //generate matrix
@@ -33,194 +30,156 @@ public class NeuralNetwork : IComparable<NeuralNetwork>
     }
 
     /// <summary>
-    /// Deep copy constructor 
+    ///     Deep copy constructor
     /// </summary>
     /// <param name="copyNetwork">Network to deep copy</param>
     public NeuralNetwork(NeuralNetwork copyNetwork)
     {
-        this.layers = new int[copyNetwork.layers.Length];
-        for (int i = 0; i < copyNetwork.layers.Length; i++)
-        {
-            this.layers[i] = copyNetwork.layers[i];
-        }
+        _layers = new int[copyNetwork._layers.Length];
+        for (var i = 0; i < copyNetwork._layers.Length; i++) _layers[i] = copyNetwork._layers[i];
 
         InitNeurons();
         InitWeights();
-        CopyWeights(copyNetwork.weights);
-    }
-
-    private void CopyWeights(float[][][] copyWeights)
-    {
-        for (int i = 0; i < weights.Length; i++)
-        {
-            for (int j = 0; j < weights[i].Length; j++)
-            {
-                for (int k = 0; k < weights[i][j].Length; k++)
-                {
-                    weights[i][j][k] = copyWeights[i][j][k];
-                }
-            }
-        }
+        CopyWeights(copyNetwork._weights);
     }
 
     /// <summary>
-    /// Create neuron matrix
-    /// </summary>
-    private void InitNeurons()
-    {
-        //Neuron Initilization
-        List<float[]> neuronsList = new List<float[]>();
-
-        for (int i = 0; i < layers.Length; i++) //run through all layers
-        {
-            neuronsList.Add(new float[layers[i]]); //add layer to neuron list
-        }
-
-        neurons = neuronsList.ToArray(); //convert list to array
-    }
-
-    /// <summary>
-    /// Create weights matrix.
-    /// </summary>
-    private void InitWeights()
-    {
-
-        List<float[][]> weightsList = new List<float[][]>(); //weights list which will later will converted into a weights 3D array
-
-        //itterate over all neurons that have a weight connection
-        for (int i = 1; i < layers.Length; i++)
-        {
-            List<float[]> layerWeightsList = new List<float[]>(); //layer weight list for this current layer (will be converted to 2D array)
-
-            int neuronsInPreviousLayer = layers[i - 1]; 
-
-            //itterate over all neurons in this current layer
-            for (int j = 0; j < neurons[i].Length; j++)
-            {
-                float[] neuronWeights = new float[neuronsInPreviousLayer]; //neruons weights
-
-                //itterate over all neurons in the previous layer and set the weights randomly between 0.5f and -0.5
-                for (int k = 0; k < neuronsInPreviousLayer; k++)
-                {
-                    //give random weights to neuron weights
-                    neuronWeights[k] = UnityEngine.Random.Range(-0.5f,0.5f);
-                }
-
-                layerWeightsList.Add(neuronWeights); //add neuron weights of this current layer to layer weights
-            }
-
-            weightsList.Add(layerWeightsList.ToArray()); //add this layers weights converted into 2D array into weights list
-        }
-
-        weights = weightsList.ToArray(); //convert to 3D array
-    }
-
-    /// <summary>
-    /// Feed forward this neural network with a given input array
-    /// </summary>
-    /// <param name="inputs">Inputs to network</param>
-    /// <returns></returns>
-    public float[] FeedForward(float[] inputs)
-    {
-        //Add inputs to the neuron matrix
-        for (int i = 0; i < inputs.Length; i++)
-        {
-            neurons[0][i] = inputs[i];
-        }
-
-        //itterate over all neurons and compute feedforward values 
-        for (int i = 1; i < layers.Length; i++)
-        {
-            for (int j = 0; j < neurons[i].Length; j++)
-            {
-                float value = 0f;
-
-                for (int k = 0; k < neurons[i-1].Length; k++)
-                {
-                    value += weights[i - 1][j][k] * neurons[i - 1][k]; //sum off all weights connections of this neuron weight their values in previous layer
-                }
-
-                neurons[i][j] = (float)Math.Tanh(value); //Hyperbolic tangent activation
-            }
-        }
-
-        return neurons[neurons.Length-1]; //return output layer
-    }
-
-    /// <summary>
-    /// Mutate neural network weights
-    /// </summary>
-    public void Mutate()
-    {
-        for (int i = 0; i < weights.Length; i++)
-        {
-            for (int j = 0; j < weights[i].Length; j++)
-            {
-                for (int k = 0; k < weights[i][j].Length; k++)
-                {
-                    float weight = weights[i][j][k];
-
-                    //mutate weight value 
-                    float randomNumber = UnityEngine.Random.Range(0f,100f);
-
-                    if (randomNumber <= 2f)
-                    { //if 1
-                      //flip sign of weight
-                        weight *= -1f;
-                    }
-                    else if (randomNumber <= 4f)
-                    { //if 2
-                      //pick random weight between -1 and 1
-                        weight = UnityEngine.Random.Range(-0.5f, 0.5f);
-                    }
-                    else if (randomNumber <= 6f)
-                    { //if 3
-                      //randomly increase by 0% to 100%
-                        float factor = UnityEngine.Random.Range(0f, 1f) + 1f;
-                        weight *= factor;
-                    }
-                    else if (randomNumber <= 8f)
-                    { //if 4
-                      //randomly decrease by 0% to 100%
-                        float factor = UnityEngine.Random.Range(0f, 1f);
-                        weight *= factor;
-                    }
-
-                    weights[i][j][k] = weight;
-                }
-            }
-        }
-    }
-
-    public void AddFitness(float fit)
-    {
-        fitness += fit;
-    }
-
-    public void SetFitness(float fit)
-    {
-        fitness = fit;
-    }
-
-    public float GetFitness()
-    {
-        return fitness;
-    }
-
-    /// <summary>
-    /// Compare two neural networks and sort based on fitness
+    ///     Compare two neural networks and sort based on fitness
     /// </summary>
     /// <param name="other">Network to be compared to</param>
     /// <returns></returns>
     public int CompareTo(NeuralNetwork other)
     {
         if (other == null) return 1;
-
-        if (fitness > other.fitness)
-            return 1;
-        else if (fitness < other.fitness)
-            return -1;
-        else
-            return 0;
+        if (_fitness > other._fitness) return 1;
+        if (_fitness < other._fitness) return -1;
+        return 0;
     }
+
+    private void CopyWeights(float[][][] copyWeights)
+    {
+        for (var i = 0; i < _weights.Length; i++)
+        for (var j = 0; j < _weights[i].Length; j++)
+        for (var k = 0; k < _weights[i][j].Length; k++)
+            _weights[i][j][k] = copyWeights[i][j][k];
+    }
+
+    /// <summary>
+    ///     Create neuron matrix
+    /// </summary>
+    private void InitNeurons()
+    {
+        //Neuron Initilization
+
+        _neurons = _layers.Select(t => new float[t]).ToArray(); //convert list to array
+    }
+
+    /// <summary>
+    ///     Create weights matrix.
+    /// </summary>
+    private void InitWeights()
+    {
+        var weightsList = new List<float[][]>(); //weights list which will later will converted into a weights 3D array
+
+        //itterate over all neurons that have a weight connection
+        for (var i = 1; i < _layers.Length; i++)
+        {
+            var layerWeightsList =
+                new List<float[]>(); //layer weight list for this current layer (will be converted to 2D array)
+
+            var neuronsInPreviousLayer = _layers[i - 1];
+
+            //itterate over all neurons in this current layer
+            for (var j = 0; j < _neurons[i].Length; j++)
+            {
+                var neuronWeights = new float[neuronsInPreviousLayer]; //neruons weights
+
+                //itterate over all neurons in the previous layer and set the weights randomly between 0.5f and -0.5
+                for (var k = 0; k < neuronsInPreviousLayer; k++)
+                    //give random weights to neuron weights
+                    neuronWeights[k] = Random.Range(-0.5f, 0.5f);
+
+                layerWeightsList.Add(neuronWeights); //add neuron weights of this current layer to layer weights
+            }
+
+            weightsList.Add(layerWeightsList
+                                .ToArray()); //add this layers weights converted into 2D array into weights list
+        }
+
+        _weights = weightsList.ToArray(); //convert to 3D array
+    }
+
+    /// <summary>
+    ///     Feed forward this neural network with a given input array
+    /// </summary>
+    /// <param name="inputs">Inputs to network</param>
+    /// <returns></returns>
+    public float[] FeedForward(float[] inputs)
+    {
+        //Add inputs to the neuron matrix
+        for (var i = 0; i < inputs.Length; i++) _neurons[0][i] = inputs[i];
+
+        //itterate over all neurons and compute feedforward values 
+        for (var i = 1; i < _layers.Length; i++)
+        for (var j = 0; j < _neurons[i].Length; j++)
+        {
+            var value = 0f;
+
+            for (var k = 0; k < _neurons[i - 1].Length; k++)
+                value += _weights[i - 1][j][k] *
+                         _neurons[i - 1]
+                             [k]; //sum off all weights connections of this neuron weight their values in previous layer
+
+            _neurons[i][j] = (float) Math.Tanh(value); //Hyperbolic tangent activation
+        }
+
+        return _neurons[_neurons.Length - 1]; //return output layer
+    }
+
+    /// <summary>
+    ///     Mutate neural network weights
+    /// </summary>
+    public void Mutate()
+    {
+        for (var i = 0; i < _weights.Length; i++)
+        for (var j = 0; j < _weights[i].Length; j++)
+        for (var k = 0; k < _weights[i][j].Length; k++)
+        {
+            var weight = _weights[i][j][k];
+
+            //mutate weight value 
+            var randomNumber = Random.Range(0f, 100f);
+
+            if (randomNumber <= 2f)
+            { //if 1
+                //flip sign of weight
+                weight *= -1f;
+            }
+            else if (randomNumber <= 4f)
+            { //if 2
+                //pick random weight between -1 and 1
+                weight = Random.Range(-0.5f, 0.5f);
+            }
+            else if (randomNumber <= 6f)
+            { //if 3
+                //randomly increase by 0% to 100%
+                var factor = Random.Range(0f, 1f) + 1f;
+                weight *= factor;
+            }
+            else if (randomNumber <= 8f)
+            { //if 4
+                //randomly decrease by 0% to 100%
+                var factor = Random.Range(0f, 1f);
+                weight *= factor;
+            }
+
+            _weights[i][j][k] = weight;
+        }
+    }
+
+    public void AddFitness(float fit) { _fitness += fit; }
+
+    public void SetFitness(float fit) { _fitness = fit; }
+
+    public float GetFitness() { return _fitness; }
 }
